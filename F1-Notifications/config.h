a91e857b49c518723055c32dd674eb826c3f5463
witnessmenow <ecenuig@gmail.com>
2024-05-06 21:08:53 +0100

    Updating UI of Race week display

--- F1-Notifications/config.h
+++ F1-Notifications/config.h
@@ -7,102 +7,116 @@
 #define F1_ROUND_OFFSET_LABEL "roundOffset"
 #define F1_CURRENT_RACE_NOTIFICATION_LABEL "currentRaceNotification"
 
-class F1Config {
-  public:
-    //How the time will be displayed, see here for more info: https://github.com/ropg/ezTime#datetime
-    String timeFormat = "l, d-M-Y H:i"; // Friday, 17-Mar-2023 00:30
-    String timeZone = "Europe/London"; //seems to be something wrong with Europe/Dublin
+class F1Config
+{
+public:
+  // How the time will be displayed, see here for more info: https://github.com/ropg/ezTime#datetime
+  String timeFormat = "l, H:i";      // Friday, 00:30
+  String timeZone = "Europe/London"; // seems to be something wrong with Europe/Dublin
 
-    // Telegram BOT Token (Get from Botfather)
-    String botToken = "";
+  // Telegram BOT Token (Get from Botfather)
+  String botToken = "";
 
-    // Use @myidbot (IDBot) to find out the chat ID of an individual or a group
-    // Also note that you need to click "start" on a bot before it can
-    // message you
-    String chatId = "";
+  // Use @myidbot (IDBot) to find out the chat ID of an individual or a group
+  // Also note that you need to click "start" on a bot before it can
+  // message you
+  String chatId = "";
 
-    int roundOffset = 0;
+  int roundOffset = 0;
 
-    bool currentRaceNotification = false;
+  bool currentRaceNotification = false;
 
-    bool isTelegramConfigured(){
-      return (botToken != "") && (chatId != "");
-    }
+  bool isTelegramConfigured()
+  {
+    return (botToken != "") && (chatId != "");
+  }
 
-    bool fetchConfigFile() {
-      if (SPIFFS.exists(F1_CONFIG_JSON)) {
-        //file exists, reading and loading
-        Serial.println("reading config file");
-        File configFile = SPIFFS.open(F1_CONFIG_JSON, "r");
-        if (configFile) {
-          Serial.println("opened config file");
-          StaticJsonDocument<1024> json;
-          DeserializationError error = deserializeJson(json, configFile);
-          serializeJsonPretty(json, Serial);
-          if (!error) {
-            Serial.println("\nparsed json");
+  bool fetchConfigFile()
+  {
+    if (SPIFFS.exists(F1_CONFIG_JSON))
+    {
+      // file exists, reading and loading
+      Serial.println("reading config file");
+      File configFile = SPIFFS.open(F1_CONFIG_JSON, "r");
+      if (configFile)
+      {
+        Serial.println("opened config file");
+        StaticJsonDocument<1024> json;
+        DeserializationError error = deserializeJson(json, configFile);
+        serializeJsonPretty(json, Serial);
+        if (!error)
+        {
+          Serial.println("\nparsed json");
 
-            if (json.containsKey(F1_TIME_ZONE_LABEL)) {
-              timeZone = String(json[F1_TIME_ZONE_LABEL].as<String>());
-            }
-
-            if (json.containsKey(F1_TIME_FORMAT_LABEL)) {
-              timeFormat = String(json[F1_TIME_FORMAT_LABEL].as<String>());
-            }
-
-            if (json.containsKey(F1_BOT_TOKEN_LABEL)) {
-              botToken = String(json[F1_BOT_TOKEN_LABEL].as<String>());
-            }
-
-            if (json.containsKey(F1_CHAT_ID_LABEL)) {
-              chatId = String(json[F1_CHAT_ID_LABEL].as<String>());
-            }
-
-            if (json.containsKey(F1_ROUND_OFFSET_LABEL)) {
-              roundOffset = json[F1_ROUND_OFFSET_LABEL].as<int>();
-            }
-
-            if (json.containsKey(F1_CURRENT_RACE_NOTIFICATION_LABEL)) {
-              currentRaceNotification = json[F1_CURRENT_RACE_NOTIFICATION_LABEL].as<bool>();
-            }
-
-            return true;
-
-          } else {
-            Serial.println("failed to load json config");
-            return false;
+          if (json.containsKey(F1_TIME_ZONE_LABEL))
+          {
+            timeZone = String(json[F1_TIME_ZONE_LABEL].as<String>());
           }
+
+          if (json.containsKey(F1_TIME_FORMAT_LABEL))
+          {
+            timeFormat = String(json[F1_TIME_FORMAT_LABEL].as<String>());
+          }
+
+          if (json.containsKey(F1_BOT_TOKEN_LABEL))
+          {
+            botToken = String(json[F1_BOT_TOKEN_LABEL].as<String>());
+          }
+
+          if (json.containsKey(F1_CHAT_ID_LABEL))
+          {
+            chatId = String(json[F1_CHAT_ID_LABEL].as<String>());
+          }
+
+          if (json.containsKey(F1_ROUND_OFFSET_LABEL))
+          {
+            roundOffset = json[F1_ROUND_OFFSET_LABEL].as<int>();
+          }
+
+          if (json.containsKey(F1_CURRENT_RACE_NOTIFICATION_LABEL))
+          {
+            currentRaceNotification = json[F1_CURRENT_RACE_NOTIFICATION_LABEL].as<bool>();
+          }
+
+          return true;
+        }
+        else
+        {
+          Serial.println("failed to load json config");
+          return false;
         }
       }
+    }
 
+    Serial.println("Config file does not exist");
+    return false;
+  }
 
-      Serial.println("Config file does not exist");
+  bool saveConfigFile()
+  {
+    Serial.println(F("Saving config"));
+    StaticJsonDocument<1024> json;
+    json[F1_TIME_ZONE_LABEL] = timeZone;
+    json[F1_TIME_FORMAT_LABEL] = timeFormat;
+    json[F1_BOT_TOKEN_LABEL] = botToken;
+    json[F1_CHAT_ID_LABEL] = chatId;
+    json[F1_ROUND_OFFSET_LABEL] = roundOffset;
+    json[F1_CURRENT_RACE_NOTIFICATION_LABEL] = currentRaceNotification;
+
+    File configFile = SPIFFS.open(F1_CONFIG_JSON, "w");
+    if (!configFile)
+    {
+      Serial.println("failed to open config file for writing");
       return false;
-
     }
 
-    bool saveConfigFile() {
-      Serial.println(F("Saving config"));
-      StaticJsonDocument<1024> json;
-      json[F1_TIME_ZONE_LABEL] = timeZone;
-      json[F1_TIME_FORMAT_LABEL] = timeFormat;
-      json[F1_BOT_TOKEN_LABEL] = botToken;
-      json[F1_CHAT_ID_LABEL] = chatId;
-      json[F1_ROUND_OFFSET_LABEL] = roundOffset;
-      json[F1_CURRENT_RACE_NOTIFICATION_LABEL] = currentRaceNotification;
-
-      File configFile = SPIFFS.open(F1_CONFIG_JSON, "w");
-      if (!configFile) {
-        Serial.println("failed to open config file for writing");
-        return false;
-      }
-
-      serializeJsonPretty(json, Serial);
-      if (serializeJson(json, configFile) == 0) {
-        Serial.println(F("Failed to write to file"));
-        return false;
-      }
-      configFile.close();
-      return true;
+    serializeJsonPretty(json, Serial);
+    if (serializeJson(json, configFile) == 0)
+    {
+      Serial.println(F("Failed to write to file"));
+      return false;
     }
+    configFile.close();
+    return true;
+  }
 };
